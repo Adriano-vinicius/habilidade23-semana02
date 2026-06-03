01ca118f17b73b846904172a8edcac64380776ed
Adriano-vinicius <viniciusadriano881@gmail.com>
2026-06-02 23:46:19 -0300

    feat: cria pacote subsystems e implementa classe modular SystemHealth

--- src/main/java/frc/robot/subsystems/SystemHealth.java
+++ src/main/java/frc/robot/subsystems/SystemHealth.java
@@ -1,16 +1,26 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class SystemHealth {
-  private String ultimoEstado = "";
-
-  public void logInicializacao() {
-    System.out.println("[SEMANA 02] robotInit: sistema inicializado");
-  }
-
-  public void logEstado(String estado) {
-    if (!estado.equals(ultimoEstado)) {
-      System.out.println("[SEMANA 02] mode: " + estado);
-      ultimoEstado = estado;
+    
+    // Construtor da classe
+    public SystemHealth() {
+        // Inicializações do subsistema, se necessário
     }
-  }
+
+    public void logInit() {
+        System.out.println("[HELLO ROBOT] Sistema iniciado com sucesso!");
+        SmartDashboard.putString("Status Robo", "Inicializado");
+    }
+
+    public void logDisabled() {
+        System.out.println("[MODO] Robo foi DESABILITADO!");
+        SmartDashboard.putString("Status Robo", "Desabilitado");
+    }
+
+    public void logTeleop() {
+        System.out.println("[MODO] Robo entrou em modo Teleoperado - Pronto para comandos!");
+        SmartDashboard.putString("Status Robo", "Teleoperado");
+    }
 }
